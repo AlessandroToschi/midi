da9ef55fc520f3ac5f386227fc977589d9d27318
Alessandro Toschi <ialessandrotoschi@gmail.com>
2016-01-28 19:58:23 +0100

    Initial Commit

---
+++ midi/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  midi
+//
+//  Created by Alessandro on 28/01/16.
+//  Copyright © 2016 Alessandro. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
